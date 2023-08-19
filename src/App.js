5a84383eba571a5d5bc52fc20b3876a82622a983
Abdulloh Ibrohimov <109219970+ibrohimov0@users.noreply.github.com>
2023-08-19 22:45:18 +0500

    3d added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
 import './App.css';
-import {useRef, useState} from "react"
 import { Canvas } from '@react-three/fiber';
-import {OrbitControls, useGLTF} from "@react-three/drei"
+import {OrbitControls, useGLTF, Stars} from "@react-three/drei"
 
+function Model(props) {
+  const { nodes, materials } = useGLTF('/shoe.gltf')
+  return (
+    <group {...props} dispose={null}>
+      <mesh geometry={nodes.shoe.geometry} material={materials.laces} />
+      <mesh geometry={nodes.shoe_1.geometry} material={materials.mesh} />
+      <mesh geometry={nodes.shoe_2.geometry} material={materials.caps} />
+      <mesh geometry={nodes.shoe_3.geometry} material={materials.inner} />
+      <mesh geometry={nodes.shoe_4.geometry} material={materials.sole} />
+      <mesh geometry={nodes.shoe_5.geometry} material={materials.stripes} />
+      <mesh geometry={nodes.shoe_6.geometry} material={materials.band} />
+      <mesh geometry={nodes.shoe_7.geometry} material={materials.patch} />
+    </group>
+  )
+}
 function App() {
-  const ref = useRef()
   return (
     <div className="App">
-      <Canvas>
-        <mesh ref={ref}>
-          <boxGeometry attach="geometry" args={[2,2,2]}/>
-        </mesh>
-      </Canvas>
+      <div className="box">
+        <div className="Model">
+          <Canvas>
+            <OrbitControls />
+            <ambientLight intensity={1.5} />
+            <spotLight position={[10,15,10]} angle={0.3} />
+            <Model/>
+          </Canvas>
+        </div>
+        <div className="Text">
+          <h1>3d Objects</h1>
+          <p>This object was created with Blender.You can see 360*.Everything looks good. :)</p>
+        </div>
+      </div>
+
     </div>
   );
 }
