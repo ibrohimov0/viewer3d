9b579cb85f697f9fd05e4d53a729d56cc48c5003
Abdulloh Ibrohimov <109219970+ibrohimov0@users.noreply.github.com>
2023-08-19 15:28:30 +0500

    setup

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import {useRef, useState} from "react"
+import { Canvas } from '@react-three/fiber';
+import {OrbitControls, useGLTF} from "@react-three/drei"
+
+function App() {
+  const ref = useRef()
+  return (
+    <div className="App">
+      <Canvas>
+        <mesh ref={ref}>
+          <boxGeometry attach="geometry" args={[2,2,2]}/>
+        </mesh>
+      </Canvas>
+    </div>
+  );
+}
+
+export default App;
